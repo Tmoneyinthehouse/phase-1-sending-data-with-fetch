ccb584a5bff5ced3b31ba2b2648c1c8a019226e3
Tyler Brown <tylerbrown487@gmail.com>
2023-05-14 23:10:33 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+        name: name,
+        email: email,
+    }
+    const post = {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json',
+            "Accept": 'application/json'
+        },
+        body: JSON.stringify(userData)
+    }
+    return fetch('http://localhost:3000/users', post)
+        .then(res => res.json())
+        .then(data => {
+            document.body.innerHTML = data.id
+        })
+
+        .catch(error => {document.body.innerHTML = error.message})
+};
